cb227fa29896c038a1e0ed5864beab701cad9224
youinwww <youinwww@gmail.com>
2023-04-29 23:01:42 +0900

    revision

--- environment/env_v2.py
+++ environment/env_v2.py
@@ -31,10 +31,11 @@ class SteelStockYard(object):
         self.num_of_retrieval_from_piles = num_of_retrieval_from_piles
 
         self.action_size = 2 * 40 + 2 + 1
-        self.state_size = {"crane": 2 * len(bays) * 44, "pile": len(bays) * 44, "plate": len(bays) * 44}
+        self.state_size = {"crane": 2 * len(bays) * 44, "plate": 2 * len(bays) * 44}
         self.meta_data = (["crane", "plate"],
                           [("crane", "interfering", "crane"),
-                           ("plate", "moving", "crane"),
+                           ("plate", "moving_rev", "crane"),
+                           ("crane", "moving", "plate"),
                            ("plate", "stacking", "plate")])
 
         if (df_storage is None) and (df_reshuffle is None) and (df_retrieval is None):
@@ -170,20 +171,21 @@ class SteelStockYard(object):
         num_of_node_for_crane = self.num_of_cranes
         num_of_node_for_plate = self.look_ahead * len(self.pile_list)
         num_of_edge_for_plate_plate = (self.look_ahead - 1) * len(self.pile_list)
+        num_of_edge_for_crane_plate = len(self.pile_list) * self.num_of_cranes
         num_of_edge_for_plate_crane = len(self.pile_list) * self.num_of_cranes
         num_of_edge_for_crane_crane = np.cumsum(range(self.num_of_cranes))[-1] * 2
 
         node_features_for_plate = np.zeros((num_of_node_for_plate, self.state_size["plate"]))
         node_features_for_crane = np.zeros((num_of_node_for_crane, self.state_size["crane"]))
 
-        mask = np.zeros((2, 41))
-        mask = np.insert(mask, 22, 1, axis=1)
-        mask = np.insert(mask, 26, 1, axis=1)
-        mask = np.insert(mask, 43, 1, axis=1)
-        mask = mask.flatten()
-        for from_pile_name in self.pile_list:
-            id = get_location_id(from_pile_name)
-            mask[id] = 1
+        # mask = np.zeros((2, 41))
+        # mask = np.insert(mask, 22, 1, axis=1)
+        # mask = np.insert(mask, 26, 1, axis=1)
+        # mask = np.insert(mask, 43, 1, axis=1)
+        # mask = mask.flatten()
+        # for from_pile_name in self.pile_list:
+        #     id = get_location_id(from_pile_name)
+        #     mask[id] = 1
 
         all_x_coords = np.array([i for i in range(1, 45)] * len(self.bays))
         all_y_coords = np.array([1 for _ in range(1, 45)] + [2 for _ in range(1, 45)])
@@ -194,50 +196,60 @@ class SteelStockYard(object):
             crane_current_y = info["Current Coord"][1]
             crane_target_x = info["Target Coord"][0]
             crane_target_y = info["Target Coord"][1]
-            features = np.maximum(2 * np.abs(all_x_coords - crane_current_x), np.abs(all_y_coords - crane_current_y))
-            features = features * mask
-            node_features_for_crane[i, :len(all_x_coords)] = features / 86
+            features_from = np.maximum(2 * np.abs(all_x_coords - crane_current_x), np.abs(all_y_coords - crane_current_y))
+            # features = features * mask
+            node_features_for_crane[i, :len(all_x_coords)] = features_from / 86
             if not (crane_target_x == -1 and crane_target_y == -1):
-                features = np.maximum(2 * np.abs(all_x_coords - crane_target_x), np.abs(all_y_coords - crane_target_y))
-                features = features * mask
-                node_features_for_crane[i, len(all_x_coords):] = features / 86
+                features_to = np.maximum(2 * np.abs(all_x_coords - crane_target_x), np.abs(all_y_coords - crane_target_y))
+                # features = features * mask
+                node_features_for_crane[i, len(all_x_coords):] = features_to / 86
 
         for i, from_pile_name in enumerate(self.pile_list):
+            from_pile_x = self.model.piles[from_pile_name].coord[0]
+            from_pile_y = self.model.piles[from_pile_name].coord[1]
+            features_from = np.maximum(2 * np.abs(all_x_coords - from_pile_x), np.abs(all_y_coords - from_pile_y))
+
             plates = self.model.piles[from_pile_name].plates
             for j in range(self.look_ahead):
                 if len(plates) >= j + 1:
                     to_pile_name = self.model.piles[from_pile_name].plates[-1 - j].to_pile
                     to_pile_x = self.model.piles[to_pile_name].coord[0]
                     to_pile_y = self.model.piles[to_pile_name].coord[1]
-                    features = np.maximum(2 * np.abs(all_x_coords - to_pile_x), np.abs(all_y_coords - to_pile_y))
-                    features = features * mask
-                    node_features_for_plate[self.look_ahead * i + j] = features / 86
+                    features_to = np.maximum(2 * np.abs(all_x_coords - to_pile_x), np.abs(all_y_coords - to_pile_y))
+                    # features = features * mask
+                    node_features_for_plate[self.look_ahead * i + j, :len(all_x_coords)] = features_from / 86
+                    node_features_for_plate[self.look_ahead * i + j, len(all_x_coords):] = features_to / 86
 
         state['crane'].x = torch.tensor(node_features_for_crane, dtype=torch.float32)
         state['plate'].x = torch.tensor(node_features_for_plate, dtype=torch.float32)
 
         edge_plate_plate = np.zeros((2, num_of_edge_for_plate_plate))
+        edge_crane_plate = np.zeros((2, num_of_edge_for_crane_plate))
         edge_plate_crane = np.zeros((2, num_of_edge_for_plate_crane))
         edge_crane_crane = np.zeros((2, num_of_edge_for_crane_crane))
 
         for i in range(self.look_ahead - 1):
             edge_plate_plate[0, i * len(self.pile_list):(i + 1) * len(self.pile_list)] \
-                = range(i + 1, self.look_ahead * len(self.pile_list), self.look_ahead)
-            edge_plate_plate[1, i * len(self.pile_list):(i + 1) * len(self.pile_list)] \
                 = range(i, self.look_ahead * len(self.pile_list), self.look_ahead)
+            edge_plate_plate[1, i * len(self.pile_list):(i + 1) * len(self.pile_list)] \
+                = range(i + 1, self.look_ahead * len(self.pile_list), self.look_ahead)
 
         for i, crane_name in enumerate(self.crane_list):
+            edge_crane_plate[0, i * len(self.pile_list):(i + 1) * len(self.pile_list)] = i
+            edge_crane_plate[1, i * len(self.pile_list):(i + 1) * len(self.pile_list)] \
+                = range(0, self.look_ahead * len(self.pile_list), self.look_ahead)
+
             edge_plate_crane[0, i * len(self.pile_list):(i + 1) * len(self.pile_list)] \
                 = range(0, self.look_ahead * len(self.pile_list), self.look_ahead)
             edge_plate_crane[1, i * len(self.pile_list):(i + 1) * len(self.pile_list)] = i
 
-        for i, crane_name in enumerate(self.crane_list):
-            edge_crane_crane[0, i * len(self.crane_list):(i + 1) * (len(self.crane_list) - 1)] = i
-            edge_crane_crane[1, i * len(self.crane_list):(i + 1) * (len(self.crane_list) - 1)] \
+            edge_crane_crane[0, i * (len(self.crane_list) - 1):(i + 1) * (len(self.crane_list) - 1)] = i
+            edge_crane_crane[1, i * (len(self.crane_list) - 1):(i + 1) * (len(self.crane_list) - 1)] \
                 = [j for j in range(len(self.crane_list)) if j != i]
 
         state['plate', 'stacking', 'plate'].edge_index = torch.tensor(edge_plate_plate, dtype=torch.long)
-        state['plate', 'moving', 'crane'].edge_index = torch.tensor(edge_plate_crane, dtype=torch.long)
+        state['crane', 'moving', 'plate'].edge_index = torch.tensor(edge_crane_plate, dtype=torch.long)
+        state['plate', 'moving_rev', 'crane'].edge_index = torch.tensor(edge_plate_crane, dtype=torch.long)
         state['crane', 'interfering', 'crane'].edge_index = torch.tensor(edge_crane_crane, dtype=torch.long)
 
         return state
