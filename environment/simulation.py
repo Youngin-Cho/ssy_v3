e8c533f2b7c054714ba3d7525d4ccc748bddb66d
youinwww <youinwww@gmail.com>
2023-08-11 16:04:31 +0900

    error revision

--- environment/simulation.py
+++ environment/simulation.py
@@ -374,8 +374,8 @@ class Management:
                 if len(self.piles[pile].plates) >= possible_dict[pile]:
                     if self.piles[pile].plates[-possible_dict[pile]].to_pile == current_to_pile:
                         same_to_pile.append(pile)
-                    target_coord = self.piles[self.piles[pile].plates[-possible_dict[pile]].to_pile].coord
-                    if len(target_coord) >= 2:
+                    if not self.piles[pile].plates[-possible_dict[pile]].to_pile in ["cn1", "cn2", "cn3"]:
+                        target_coord = self.piles[self.piles[pile].plates[-possible_dict[pile]].to_pile].coord
                         if abs(target_coord[0] -self.piles[current_to_pile].coord[0]) <= self.multi_dist:
                             if weight + self.piles[pile].plates[-possible_dict[pile]].w <= self.multi_weight:
                                 possible_action.append(pile)
@@ -386,8 +386,8 @@ class Management:
             if len(self.piles[action].plates) >= possible_dict[action] + 1:
                 if self.piles[action].plates[-possible_dict[action] - 1].to_pile == current_to_pile:
                     same_to_pile.append(action)
-                target_coord = self.piles[self.piles[action].plates[-possible_dict[action] - 1].to_pile].coord
-                if len(target_coord) >= 2:
+                if not self.piles[pile].plates[-possible_dict[pile]].to_pile in ["cn1", "cn2", "cn3"]:
+                    target_coord = self.piles[self.piles[action].plates[-possible_dict[action] - 1].to_pile].coord
                     if abs(target_coord[0] - self.piles[current_to_pile].coord[0]) <= self.multi_dist:
                         if weight + self.piles[action].plates[-possible_dict[action] - 1].w <= self.multi_weight:
                             possible_action.append(action)
