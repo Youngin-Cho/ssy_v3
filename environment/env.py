baace5ab0b8885832cef2bee8d272e6d72c74ba8
Youngin-Cho <youinwww@gmail.com>
2023-10-23 15:10:33 +0900

    state revision

--- environment/env.py
+++ environment/env.py
@@ -36,7 +36,7 @@ class SteelStockYard:
             self.df_retrieval = pd.read_excel(self.data_src, sheet_name="retrieval", engine="openpyxl")
 
         self.action_size = 2 * 40 + 2 + 1
-        self.state_size = {"crane": 2, "pile": 1 + 1 * look_ahead}
+        self.state_size = {"crane": 2, "pile": 1 + 2 * look_ahead}
         self.meta_data = (["crane", "pile"],
                           [("crane", "interfering", "crane"),
                            ("pile", "moving_rev", "crane"),
@@ -253,9 +253,9 @@ class SteelStockYard:
                     plate = self.model.piles[from_pile_name].plates[-1-j]
                     to_pile_name = plate.to_pile
                     to_pile_x = self.model.piles[to_pile_name].coord[0]
-                    # weight = plate.w
+                    weight = plate.w
                     node_features_for_pile[i, 1 * j + 1] = to_pile_x / 44
-                    # node_features_for_pile[i, 2 * j + 2] = weight / 19.294
+                    node_features_for_pile[i, 2 * j + 2] = weight / 19.294
 
         state['crane'].x = torch.tensor(node_features_for_crane, dtype=torch.float32)
         state['pile'].x = torch.tensor(node_features_for_pile, dtype=torch.float32)
