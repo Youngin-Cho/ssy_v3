6e61cb31fc1b3869c93c3790e0a6783e73cbaecc
Youngin-Cho <youinwww@gmail.com>
2023-11-24 13:31:05 +0900

    evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from scipy import stats
 
-if __name__ == "__main__":
-    file_path = "./output/test/case0/test_results.xlsx"
+
+def draw_boxplot(file_path):
     df = pd.read_excel(file_path)
     df = df.iloc[:-1]
 
@@ -13,11 +14,25 @@ if __name__ == "__main__":
     ax.set_xticks([1, 2, 3, 4], ['RL', 'SD', 'MA', 'RAND'])
     ax.set_ylabel('Makespan')
 
-    # plt.subplots_adjust(left=0.2)
-    plt.show()
-
     fig.savefig("./boxplot.png")
 
 
+def perform_paired_t_test(file_path):
+    df = pd.read_excel(file_path)
+    df = df.iloc[:-1]
+
+    res = stats.ttest_rel(df["RL"], df["SD"])
+    print("(RL vs SD) p-value: %f" % res.pvalue)
+
+    res = stats.ttest_rel(df["RL"], df["MA"])
+    print("(RL vs MA) p-value: %f" % res.pvalue)
+
+    res = stats.ttest_rel(df["RL"], df["RAND"])
+    print("(RL vs RAND) p-value: %f" % res.pvalue)
+
+
+if __name__ == "__main__":
+    file_path = "./output/test/case0/test_results.xlsx"
+    perform_paired_t_test(file_path)
 
 
